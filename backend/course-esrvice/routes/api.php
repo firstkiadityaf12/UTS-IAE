7493e116db5f69bead50fe6a65457d76d26ca235
padelly <mf273404@gmail.com>
2025-04-25 21:40:34 +0700

    commit

--- backend/course-esrvice/routes/api.php
+++ backend/course-esrvice/routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
-use App\Http\Controllers\CourseController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
-Route::get('/courses', [CourseController::class, 'index']);
-Route::post('/courses', [CourseController::class, 'store']);
-Route::get('/courses/{id}', [CourseController::class, 'show']);
-Route::put('/courses/{id}', [CourseController::class, 'update']);
-Route::delete('/courses/{id}', [CourseController::class, 'destroy']); 
+Route::middleware('api')->group(function () {
+    Route::get('/check', function () {
+        return response()->json(['message' => 'API is working']);
+    });
+
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::post('/courses', [CourseController::class, 'store']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
+    Route::put('/courses/{id}', [CourseController::class, 'update']);
+    Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
+});
--- backend/course-esrvice/routes/web.php
+++ backend/course-esrvice/routes/web.php
@@ -7,4 +7,5 @@ Route::get('/courses', [CourseController::class, 'index']);
 Route::post('/courses', [CourseController::class, 'store']);
 Route::get('/courses/{id}', [CourseController::class, 'show']);
 Route::put('/courses/{id}', [CourseController::class, 'update']);
-Route::delete('/courses/{id}', [CourseController::class, 'destroy']); 
+Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
+ 
