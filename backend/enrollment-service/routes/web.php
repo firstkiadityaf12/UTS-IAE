92f3238956fac430ce131d3d1ea2a740b5c4277e
Raykeisha Ridhan Taruna <reykeishaa@gmail.com>
2025-04-27 16:19:06 +0700

    Update web.php

--- backend/enrollment-service/routes/web.php
+++ backend/enrollment-service/routes/web.php
@@ -3,4 +3,10 @@
 use App\Http\Controllers\EnrollmentController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('enrollments', EnrollmentController::class);
+Route::prefix('api')->group(function () {
+    Route::get('/enrollments', [EnrollmentController::class, 'index']);
+    Route::post('/enrollments', [EnrollmentController::class, 'store']);
+    Route::get('/enrollments/{id}', [EnrollmentController::class, 'show']);
+    Route::put('/enrollments/{id}', [EnrollmentController::class, 'update']);
+    Route::delete('/enrollments/{id}', [EnrollmentController::class, 'destroy']);
+});
