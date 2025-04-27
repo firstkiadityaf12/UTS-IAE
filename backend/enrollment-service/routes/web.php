eea050c6dd7f0328cac48511c09c1677872a09ff
Raykeisha Ridhan Taruna <reykeishaa@gmail.com>
2025-04-27 17:38:25 +0700

    www

--- backend/enrollment-service/routes/web.php
+++ backend/enrollment-service/routes/web.php
@@ -7,10 +7,9 @@ Route::get('/check', function () {
     return response()->json(['message' => 'API is working']);
 });
 
-Route::prefix('api')->group(function () {
-    Route::get('/enrollments', [EnrollmentController::class, 'index']);
-    Route::post('/enrollments', [EnrollmentController::class, 'store']);
-    Route::get('/enrollments/{id}', [EnrollmentController::class, 'show']);
-    Route::put('/enrollments/{id}', [EnrollmentController::class, 'update']);
-    Route::delete('/enrollments/{id}', [EnrollmentController::class, 'destroy']);
-});
+
+Route::get('/enrollments', [EnrollmentController::class, 'index']);
+Route::post('/enrollments', [EnrollmentController::class, 'store']);
+Route::get('/enrollments/{id}', [EnrollmentController::class, 'show']);
+Route::put('/enrollments/{id}', [EnrollmentController::class, 'update']);
+Route::delete('/enrollments/{id}', [EnrollmentController::class, 'destroy']);
