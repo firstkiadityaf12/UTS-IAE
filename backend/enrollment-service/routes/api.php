c1139c12242fb06ee9db2fc4a7307cce8740f4e6
Raykeisha Ridhan Taruna <reykeishaa@gmail.com>
2025-04-27 16:48:47 +0700

    ww

--- backend/enrollment-service/routes/api.php
+++ backend/enrollment-service/routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\EnrollmentController;
-use Illuminate\Support\Facades\Route;
 
-Route::apiResource('enrollments', EnrollmentController::class);
+
+Route::get('/enrollments', [EnrollmentController::class, 'index']);
+Route::post('/enrollments', [EnrollmentController::class, 'store']);
+Route::get('/enrollments/{id}', [EnrollmentController::class, 'show']);
+Route::put('/enrollments/{id}', [EnrollmentController::class, 'update']);
+Route::delete('/enrollments/{id}', [EnrollmentController::class, 'destroy']);
+
--- backend/enrollment-service/routes/web.php
+++ backend/enrollment-service/routes/web.php
@@ -1,12 +1,16 @@
 <?php
-
 use App\Http\Controllers\EnrollmentController;
 use Illuminate\Support\Facades\Route;
+Route::middleware('api')->group(function () {
+    Route::get('/check', function () {
+        return response()->json(['message' => 'API is working']);
+    });
 
-Route::prefix('api')->group(function () {
     Route::get('/enrollments', [EnrollmentController::class, 'index']);
     Route::post('/enrollments', [EnrollmentController::class, 'store']);
     Route::get('/enrollments/{id}', [EnrollmentController::class, 'show']);
     Route::put('/enrollments/{id}', [EnrollmentController::class, 'update']);
     Route::delete('/enrollments/{id}', [EnrollmentController::class, 'destroy']);
 });
+
+
