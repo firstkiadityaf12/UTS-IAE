b697c07577861296b63595676b10b6e1fa3b6c55
firstkiadityaf12 <adityafernanda2013@gmail.com>
2025-06-12 19:40:14 +0700

    feat(graphql_teacher): add new teacher grpahql

--- backend/teacher-service/routes/api.php
+++ backend/teacher-service/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
+use Nuwave\Lighthouse\Support\Facades\GraphQL;
+
+Route::post('/graphql', [\Nuwave\Lighthouse\Support\Http\Controllers\GraphQLController::class, 'query']);
 
 Route::get('/teacher', function () {
     return response()->json(['message' => 'API is working']);
